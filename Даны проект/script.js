e51adb4b579918fb7b6a009d05c50edf3cdfbdf9
DaniellaGull <107505908+DaniellaGull@users.noreply.github.com>
2022-06-14 21:59:35 +0300

    Add files via upload

---
+++ Даны проект/script.js
@@ -0,0 +1,6 @@
+let showBtns = document.querySelector('.show')
+
+let btn = document.querySelector('#fast');
+btn.addEventListener('click',function(){
+    showBtns.classList.toggle('third__shown')
+})
